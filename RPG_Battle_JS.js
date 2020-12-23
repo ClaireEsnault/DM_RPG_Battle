8e8f0c89a74f504599ddae3d8fffd45a5b50a359
ClaireEsnault <74654965+ClaireEsnault@users.noreply.github.com>
2020-12-23 17:32:03 +0100

    test3

--- RPG_Battle_JS.js
+++ RPG_Battle_JS.js
@@ -1,5 +1,15 @@
 var monstre1 = document.getElementById("Monstre1")
+var monstre2 = document.getElementById("Monstre2")
+var monstre3 = document.getElementById("Monstre3")
 
 monstre1.onmouseover = function(){
 	document.getElementById("infobulle1").style.visibility = "visible"
+}
+
+monstre2.onmouseover = function(){
+	document.getElementById("infobulle2").style.visibility = "visible"
+}
+
+monstre3.onmouseover = function(){
+	document.getElementById("infobulle3").style.visibility = "visible"
 }
