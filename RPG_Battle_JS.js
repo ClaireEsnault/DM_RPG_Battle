92d0d4aa20344c09327ef915721178363150a5fa
ClaireEsnault <74654965+ClaireEsnault@users.noreply.github.com>
2020-12-23 17:57:19 +0100

    test4

--- RPG_Battle_JS.js
+++ RPG_Battle_JS.js
@@ -1,6 +1,12 @@
 var monstre1 = document.getElementById("Monstre1")
 var monstre2 = document.getElementById("Monstre2")
 var monstre3 = document.getElementById("Monstre3")
+var attaquer = document.getElementById("Attaquer")
+var défendre = document.getElementById("Défense")
+var endormir = document.getElementById("AttaqueSpé1")
+var assommer = document.getElementById("AttaqueSpé2")
+var soin = document.getElementById("AttaqueSpé3")
+var glace = document.getElementById("AttaqueSpé4")
 
 monstre1.onmouseover = function(){
 	document.getElementById("infobulle1").style.visibility = "visible"
